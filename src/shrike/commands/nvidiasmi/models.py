311d3b0ebef4f9fbce642295871c04e3905f8f3f
Elia Palme <elia.palme@cscs.ch>
2025-03-17 11:12:53 +0100

    updated nv gpu model

--- src/shrike/commands/nvidiasmi/models.py
+++ src/shrike/commands/nvidiasmi/models.py
@@ -1,13 +1,13 @@
-from typing import List
+from typing import List, Optional
 from pydantic import BaseModel
                   
 class GPUInfo(BaseModel):
     id:str
-    module_id:int
+    module_id:Optional[int]
     model:str
     temp:int
-    memory_total:int
-    memory_used:int               
+    memory_total:Optional[int]
+    memory_used:Optional[int]               
 
 class NvidiaSMIOutput(BaseModel):
    gpus:List[GPUInfo]
