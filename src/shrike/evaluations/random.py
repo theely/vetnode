7bd8a9954235a4d363e36d291b06cd47dcd16e3d
Elia Palme <elia.palme@cscs.ch>
2025-03-17 13:46:25 +0100

    Added random

---
+++ src/shrike/evaluations/random.py
@@ -0,0 +1,15 @@
+
+from typing import Literal
+import random
+from shrike.evaluations.base_eval import BaseEval
+from shrike.evaluations.models import Evaluation
+
+
+class RandomEval(BaseEval):
+    name:str
+    type: Literal["random"]
+
+    async def eval(self)->Evaluation:
+        eval:Evaluation = super().eval()
+        eval.passed = random.choice([True, False])
+        return eval
