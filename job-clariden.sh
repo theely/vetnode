130ce4841792f96f16ec7d172f693344a2d53bbc
Elia Palme <elia.palme@cscs.ch>
2025-03-18 09:05:17 +0100

    sanity ouput in home folder

--- job-clariden.sh
+++ job-clariden.sh
@@ -35,14 +35,15 @@ source .venv-shrike/bin/activate
 python -m pip --no-cache-dir install --upgrade pip
 pip install --no-cache-dir -r ./requirements.txt
 cd src
-touch sanity-results.txt
+rm /users/palmee/shrike-deploy/sanity-results.txt
+touch /users/palmee/shrike-deploy/sanity-results.txt
 
 # Run nodes vetting
-srun python -m shrike diagnose ../templates/simple-config.yaml >> sanity-results.txt
+srun python -m shrike diagnose ../templates/simple-config.yaml >> /users/palmee/shrike-deploy/sanity-results.txt
 
 # Extract node lists
-grep '^Cordon:' sanity-results.txt | awk '{print $2}' > cordoned-nodes.txt
-grep '^Vetted:' sanity-results.txt | awk '{print $2}' > vetted-nodes.txt
+grep '^Cordon:' /users/palmee/shrike-deploy/sanity-results.txt | awk '{print $2}' > cordoned-nodes.txt
+grep '^Vetted:' /users/palmee/shrike-deploy/sanity-results.txt | awk '{print $2}' > vetted-nodes.txt
 
 #Run on healthy nodes only
 if [ $(wc -l < vetted-nodes.txt) -ge $REQUIRED_NODES ]; then
