be2a0f3473591964dc4acafe9d3a5979beeda7dd
Elia Palme <elia.palme@cscs.ch>
2025-05-12 16:10:05 +0200

    removed fix localhost

--- src/vetnode/evaluations/cuda_nccl_eval.py
+++ src/vetnode/evaluations/cuda_nccl_eval.py
@@ -54,7 +54,6 @@ class CUDANCCLEval(BaseEval):
                 local_rank = int(os.environ["SLURM_LOCALID"])
                 nodes = asyncio.run(ScontrolCommand().run()).hostnames
                 master_node = nodes[0]
-                master_node='localhost'
                 world_size = int(os.environ['SLURM_NTASKS'])
             case _:
                 raise NotImplementedError("Support for the rquested scheduler has not been implemented.")
