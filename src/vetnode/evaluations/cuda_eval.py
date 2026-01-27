0f4ec98439ce534b83e03042535ed23b93bd5528
Elia Palme <elia.palme@cscs.ch>
2026-01-27 17:02:18 +0100

    preload libcuda

--- src/vetnode/evaluations/cuda_eval.py
+++ src/vetnode/evaluations/cuda_eval.py
@@ -36,13 +36,14 @@ class CUDAEval(BaseEval):
     cuda_home: str
 
     def verify(self)->bool:
-        #libc = CDLL(f"{self.cuda_home}/lib64/libnvrtc.so")
-        #if libc is None:
-        #    return False
-        #for filename in os.listdir(f"{self.cuda_home}/lib64"):
-        #    if filename.endswith(".so"):
-        #        lib_path = os.path.join(f"{self.cuda_home}/lib64", filename)
-        #        CDLL(f"{lib_path}")
+        libc = CDLL(f"{self.cuda_home}/lib64/libnvrtc.so")
+        if libc is None:
+            return False
+        CDLL(f"{self.cuda_home}/compat/libcuda.so")
+        for filename in os.listdir(f"{self.cuda_home}/lib64"):
+            if filename.endswith(".so"):
+                lib_path = os.path.join(f"{self.cuda_home}/lib64", filename)
+                CDLL(f"{lib_path}")
         return True
 
 
@@ -96,7 +97,7 @@ class CUDAEval(BaseEval):
         ctxParams = driver.CUctxCreateParams()  # Default initialized
         err, context = driver.cuCtxCreate(ctxParams,0, cuDevice)
         self.checkCudaErrors(err)
-
+        
         err, = driver.cuCtxSetCurrent(context)
         self.checkCudaErrors(err)
 
